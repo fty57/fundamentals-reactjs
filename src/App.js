4dc61d36ed4969b15aafb05a84d029e98d753402
fty57 <fty.57@alu.ufc.br>
2022-12-23 09:44:06 -0300

    Componentes de classe com estado (stateful)

--- src/App.js
+++ src/App.js
@@ -3,21 +3,46 @@ import "./App.css";
 import { Component } from "react";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    // Que esse atributo, recebe esse método, assim permitindo ele ter o this dentro
+    this.state = {
+      name: "Francisco Álisson",
+      counter: 0,
+    };
+  }
+
+  handlePClick = () => {
+    this.setState({ name: "Junior" });
+  };
+
+  handleAClick = (event) => {
+    // A arrowfunction não tem this
+    event.preventDefault(); // Prevenir o comportamento padrão daquela função
+    const { counter } = this.state;
+    const nextCounter = counter + 1;
+    this.setState({ counter: nextCounter });
+  };
+
+  // Mudou o estado, o render vai ser chamado novamente
   render() {
+    const { name, counter } = this.state;
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+          <p onClick={this.handlePClick}>
+            {name} {counter}
           </p>
           <a
+            onClick={this.handleAClick}
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Este é o link
           </a>
         </header>
       </div>
