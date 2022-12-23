44e20c6eea49e5f1bcaba95776d048077f979ba7
fty57 <fty.57@alu.ufc.br>
2022-12-23 09:53:00 -0300

    State com arrays e objetos

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Component } from "react";
 
@@ -9,42 +8,38 @@ class App extends Component {
     this.state = {
       name: "Francisco Álisson",
       counter: 0,
+      posts: [
+        {
+          id: 1,
+          title: "O título 1",
+          body: "O corpo 1",
+        },
+        {
+          id: 2,
+          title: "O título 2",
+          body: "O corpo 2",
+        },
+        {
+          id: 3,
+          title: "O título 3",
+          body: "O corpo 3",
+        },
+      ],
     };
   }
 
-  handlePClick = () => {
-    this.setState({ name: "Junior" });
-  };
-
-  handleAClick = (event) => {
-    // A arrowfunction não tem this
-    event.preventDefault(); // Prevenir o comportamento padrão daquela função
-    const { counter } = this.state;
-    const nextCounter = counter + 1;
-    this.setState({ counter: nextCounter });
-  };
-
   // Mudou o estado, o render vai ser chamado novamente
   render() {
-    const { name, counter } = this.state;
+    const { posts } = this.state;
 
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p onClick={this.handlePClick}>
-            {name} {counter}
-          </p>
-          <a
-            onClick={this.handleAClick}
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Este é o link
-          </a>
-        </header>
+        {posts.map((post) => (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+          </div>
+        ))}
       </div>
     );
   }
